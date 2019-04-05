7273fd638bc4320600eeea9c6e99213afcb0fa96
Pavel Khankevich <crystalfaster@gmail.com>
2019-04-05 16:03:22 +0300

    Add gradle files for lombok, testIntegration and docker-postgres-flyway-jooq with generateJooq task Add base schema for flyway

--- src/main/java/com/johncnstn/Application.java
+++ src/main/java/com/johncnstn/Application.java
@@ -1,13 +1,13 @@
-package com.johncnstn.gradleswaggerflywayjooq;
+package com.johncnstn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GradleSwaggerFlywayJooqApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GradleSwaggerFlywayJooqApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }

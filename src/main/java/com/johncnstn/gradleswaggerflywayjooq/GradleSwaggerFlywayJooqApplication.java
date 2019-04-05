e6dab0543bb949aa8f9cb396e61d4cf72cb035f8
Pavel Khankevich <crystalfaster@gmail.com>
2019-04-05 14:55:11 +0300

    Initial commit

---
+++ src/main/java/com/johncnstn/gradleswaggerflywayjooq/GradleSwaggerFlywayJooqApplication.java
@@ -0,0 +1,13 @@
+package com.johncnstn.gradleswaggerflywayjooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSwaggerFlywayJooqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSwaggerFlywayJooqApplication.class, args);
+	}
+
+}
